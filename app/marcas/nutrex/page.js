8564db35f5580687dcbd0c79844a0f4481d54da1
SuplementosVenezuela <suplementosvenezuela@gmail.com>
2023-09-08 20:44:19 -0400

    Textos 2 boton comprar por marca y producto  hasta 09/03/23

--- app/marcas/nutrex/page.js
+++ app/marcas/nutrex/page.js
@@ -604,7 +604,7 @@ export default function Produtos() {
         </div>
         <div className="px-5 grid gap-1 grid-cols-1 lg:gap-4 lg:grid-cols-2 w-screen h-auto">
           {cards.map((card, index) => {
-            const whatsappLink = `https://api.whatsapp.com/send?phone=584142743886&text=${encodeURIComponent(`¡Hola! Saludos, 👋
+            const whatsappLink = `https://api.whatsapp.com/send?phone=584140124810&text=${encodeURIComponent(`¡Hola! Saludos, 👋
             Les escribo a través de su página web SuplementosVenezuela.com desde la categoría marca "Nutrex" y estoy interesado en obtener más información y comprar "${card.producto}" por el precio de: ${card.precio} y también en recibir asesoría personalizada para el uso del mismo. Gracias de antemano. Mi nombre es: `)}`;
             return (
               <div key={index} className="flex flex-col w-full py-5 px-1 border-black mx-auto border-4 2 hover:bg-gradient-to-b from-black to-white/50 sm:w-4/5">
